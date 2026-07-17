3eda4fb6a2fd5b3fd0fba27a0a6b72d509045aeb
RenatoMoraesBarbosa <renatomoraesbarbosa72@gmail.com>
2026-07-16 22:30:08 -0300

    Adicionei o botão remover
    Adicionei o botão remover no carrinho.

--- compras.js
+++ compras.js
@@ -34,7 +34,7 @@ botao.addEventListener("click", function() {
     const lista = document.getElementById("lista");
     lista.innerText = "";
 
-    produtos.forEach((item, indice) => {
+    produtos.forEach((item) => {
         const div = document.createElement("div");
 
         const nome = document.createElement("h2");
@@ -64,87 +64,66 @@ botao.addEventListener("click", function() {
         const carrinho = document.getElementById("carrinho");
 
         comprar.addEventListener("click", function() {
-            if(item.estoque > 0) {
+
+            if (item.estoque > 0) {
+
                 item.estoque--;
                 estoque.innerText = `Estoque: ${item.estoque}`;
 
                 const item_carrinho = document.createElement("div");
 
-<<<<<<< Updated upstream
                 const texto = document.createElement("span");
-
                 texto.innerText = `Produto: ${item.nome} - R$ ${item.preco}`;
 
                 const remover = document.createElement("button");
-
                 remover.innerText = "Remover";
 
                 item_carrinho.appendChild(texto);
                 item_carrinho.appendChild(remover);
+
                 carrinho.appendChild(item_carrinho);
 
                 remover.addEventListener("click", function() {
+
                     item.estoque++;
                     estoque.innerText = `Estoque: ${item.estoque}`;
 
                     const indicePreco = valor_compra.indexOf(item.preco);
+
                     if (indicePreco !== -1) {
                         valor_compra.splice(indicePreco, 1);
                     }
 
                     item_carrinho.remove();
+
                 });
 
                 valor_compra.push(item.preco);
+
             } else {
+
                 alert("Produto sem estoque!");
+
             }
+
         });
+
     });
+
 });
-=======
-            const item_carrinho = document.createElement("div");
-
-            const texto = document.createElement("span"); 
-
-            texto.innerText = `Produto: ${item.nome} - R$ ${item.preco}`; 
-
-            const remover = document.createElement("button"); 
-
-            remover.innerText = "Remover"; 
-
-            item_carrinho.appendChild(texto); 
-            item_carrinho.appendChild(remover); 
-            carrinho.appendChild(item_carrinho);
-
-            remover.addEventListener("click", function() { 
-                item.estoque++; 
-                estoque.innerText = `Estoque: ${item.estoque}`; 
-
-                const indicePreco = valor_compra.indexOf(item.preco);
-                if (indicePreco !== -1) {
-                    valor_compra.splice(indicePreco, 1);
-                }
-                item_carrinho.remove(); 
-            })
-            valor_compra.push(item.preco);
-        } else {
-            alert("Produto sem estoque!");
-        }
-    });
-});
-})
->>>>>>> Stashed changes
 
 const pagamento = document.getElementById("pagamento");
 
 pagamento.addEventListener("click", function() {
+
     let soma = 0;
+
     const total = document.getElementById("total");
 
-    for(let i = 0; i < valor_compra.length; i++) {
+    for (let i = 0; i < valor_compra.length; i++) {
         soma += valor_compra[i];
     }
 
     total.innerText = `O valor da compra é: ${soma}`;
+
 });
